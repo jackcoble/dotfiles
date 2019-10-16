7cb8e93a56e0ae433a0824ef00935033ac38a32c
Jack Coble <jackkdev@protonmail.com>
2019-10-16 22:04:49 +0100

    dotfiles: symlink for wallpaper

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ mkdir -p $HOME/.config/git
 mkdir -p $HOME/.config/nvim
 mkdir -p $HOME/.config/rofi
 mkdir -p $HOME/.config/sxhkd
+mkdir -p $HOME/.config/wallpaper
 
 # file symlinks
 echo "setting config symlinks"
@@ -17,6 +18,7 @@ ln -sf "$DOTFILES_HOME"/config/git/config $HOME/.config/git/config
 ln -sf "$DOTFILES_HOME"/config/nvim/init.vim $HOME/.config/nvim/init.vim
 ln -sf "$DOTFILES_HOME"/config/rofi/rofi.conf $HOME/.config/rofi/rofi.conf
 ln -sf "$DOTFILES_HOME"/config/sxhkd/sxhkdrc $HOME/.config/sxhkd/sxhkdrc
+ln -sf "$DOTFILES_HOME"/config/wallpaper/default $HOME/.config/wallpaper/default
 ln -sf "$DOTFILES_HOME"/config/tmux/tmux.conf $HOME/.tmux.conf
 ln -sf "$DOTFILES_HOME"/config/xorg/xinitrc $HOME/.xinitrc
 
