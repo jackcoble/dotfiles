cc374e5316604309cd8e6ce31fb86658995be3db
Jack Coble <jackkdev@protonmail.com>
2019-10-21 19:18:34 +0100

    dotfiles: remove yabar

--- install.sh
+++ install.sh
@@ -12,7 +12,6 @@ mkdir -p $HOME/.config/nvim
 mkdir -p $HOME/.config/rofi
 mkdir -p $HOME/.config/sxhkd
 mkdir -p $HOME/.config/wallpaper
-mkdir -p $HOME/.config/yabar
 
 # file symlinks
 echo "setting config symlinks"
@@ -22,7 +21,6 @@ ln -sf "$DOTFILES_HOME"/config/nvim/init.vim $HOME/.config/nvim/init.vim
 ln -sf "$DOTFILES_HOME"/config/rofi/rofi.conf $HOME/.config/rofi/rofi.conf
 ln -sf "$DOTFILES_HOME"/config/sxhkd/sxhkdrc $HOME/.config/sxhkd/sxhkdrc
 ln -sf "$DOTFILES_HOME"/config/wallpaper/default $HOME/.config/wallpaper/default
-ln -sf "$DOTFILES_HOME"/config/yabar/yabar.conf $HOME/.config/yabar/yabar.conf
 ln -sf "$DOTFILES_HOME"/config/tmux/tmux.conf $HOME/.tmux.conf
 ln -sf "$DOTFILES_HOME"/config/xorg/xinitrc $HOME/.xinitrc
 ln -sf "$DOTFILES_HOME"/config/sh/bashrc $HOME/.bashrc
