5e8fc8a54e76fcf22cd274a30b96c000503483eb
Jack Coble <jackcoble@tutanota.com>
2020-05-09 14:13:46 +0100

    scripts: use rsync to backup to remote server

--- scripts/syncthingBackup.sh
+++ scripts/syncthingBackup.sh
@@ -2,18 +2,16 @@
 
 # Variables
 MOUNT_POINT=/mnt/SG4TB1
-ENDPOINT=https://S3_INSTANCE_URL
-S3_FOLDER=syncthing-backup
-
-# Stop the Syncthing server to avoid any potential issues with corruption
-systemctl stop pi@syncthing.service
+RSYNC_HOST=user@server
+RSYNC_REMOTE_DIR=/mnt/SG4TB1/Backups/Syncthing
+SSH_PORT=XXXXX
 
 # Navigate to Syncthing data directory
 echo "======================================"
 echo "Backing up important Syncthing directories"
 cd $MOUNT_POINT/Syncthing
 
-SYNCTHING_FILE_NAME=$(date +"%d_%m_%Y")_syncthing_backup
+SYNCTHING_FILE_NAME=$(date +"%d-%m-%Y-%T")-syncthing_backup
 echo $SYNCTHING_FILE_NAME
 
 # Compress the folder into a tarball
@@ -24,12 +22,10 @@ tar czf $MOUNT_POINT/$SYNCTHING_FILE_NAME.tar.gz *
 echo "Encrypting"
 gpg -o $MOUNT_POINT/$SYNCTHING_FILE_NAME.tar.gz.gpg -e -r jackcoble@tutanota.com $MOUNT_POINT/$SYNCTHING_FILE_NAME.tar.gz
 
-# Upload to S3 server
-echo "Uploading to $ENDPOINT"
-aws s3 cp $MOUNT_POINT/$SYNCTHING_FILE_NAME.tar.gz.gpg s3://$S3_FOLDER/$SYNCTHING_FILE_NAME.tar.gz.gpg --endpoint-url=$ENDPOINT
+# Rsync to server
+echo "Uploading to $RSYNC_HOST"
+rsync -avz -e "ssh -p $SSH_PORT" $MOUNT_POINT/$SYNCTHING_FILE_NAME.tar.gz.gpg $RSYNC_HOST:$RSYNC_REMOTE_DIR
 
 # Shred the backup tarball (both unencrypted and encrypted version)
-# Restart the Syncthing server
 echo "Cleaning up..."
 shred -u $MOUNT_POINT/$SYNCTHING_FILE_NAME.tar.gz $MOUNT_POINT/$SYNCTHING_FILE_NAME.tar.gz.gpg
-systemctl start pi@syncthing.service
