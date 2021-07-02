6dee0b7cb018fd205c72e29de202fbdfcef3de58
Jack Coble <jack@coble.uk>
2021-07-02 15:45:39 +0100

    Android SDK environment vars

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -17,5 +17,14 @@ set -x PATH $PATH /usr/local/go/bin $GOPATH/bin
 # NPM
 set PATH $HOME/.npm-global/bin $PATH
 
+# Android SDK
+set --export ANDROID $HOME/Library/Android;
+set --export ANDROID_HOME $ANDROID/sdk;
+set -gx PATH $ANDROID_HOME/tools $PATH;
+set -gx PATH $ANDROID_HOME/tools/bin $PATH;
+set -gx PATH $ANDROID_HOME/platform-tools $PATH;
+set -gx PATH $ANDROID_HOME/emulator $PATH
+
 # Dotfiles bare repo alias
 alias dots="/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
+set -g fish_user_paths "/usr/local/opt/openjdk/bin" $fish_user_paths
