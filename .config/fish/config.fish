b7b4a511be744a026d2b880e29989c46c3638af6
Jack Coble <jack@coble.uk>
2023-12-11 22:30:38 +0000

    Changes to Fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,13 +7,22 @@ source $HOME/.config/fish/gnupg.fish
 # Aliases
 # AWS CLI (Load in credentials from Pass)
 function load_aws_cli
-  set -x AWS_ACCESS_KEY_ID (pass show Development/AWS/Personal | sed -n 's/access-key-id: //p')
-  set -x AWS_SECRET_ACCESS_KEY (pass show Development/AWS/Personal | sed -n 's/secret-access-key: //p')
+  set -Ux AWS_ACCESS_KEY_ID (pass show Development/AWS/Personal | sed -n 's/access-key-id: //p')
+  set -Ux AWS_SECRET_ACCESS_KEY (pass show Development/AWS/Personal | sed -n 's/secret-access-key: //p')
 
   command aws $argv
 end
 
+# AWS CDK (Load in credentials from Pass)
+function load_aws_cdk
+  set -x AWS_ACCESS_KEY_ID (pass show Development/AWS/Personal | sed -n 's/access-key-id: //p')
+  set -x AWS_SECRET_ACCESS_KEY (pass show Development/AWS/Personal | sed -n 's/secret-access-key: //p')
+
+  command cdk $argv
+end
+
 alias aws="load_aws_cli"
+alias cdk="load_aws_cdk"
 
 # Go
 set -x GOPATH $HOME/go
@@ -22,6 +31,9 @@ set -x PATH $PATH /usr/local/go/bin $GOPATH/bin
 # NPM and Yarn
 set PATH $HOME/.npm-global/bin $PATH
 set PATH (yarn global bin) $PATH
+
+# Random scripts/binaries
+set PATH $HOME/bin $PATH
 
 # Dotfiles bare repo alias
 alias dots="/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
