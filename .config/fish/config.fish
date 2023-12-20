ee5a3005ef812722e22ae23341059b62d1c7794d
Jack Coble <jack@coble.uk>
2023-12-20 16:30:35 +0000

    Refactor Fish config and use 1Password SSH agent

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,36 +1,12 @@
 # Disable welcome prompt
 set -U fish_greeting
 
-# Load in GPG agent
-source $HOME/.config/fish/gnupg.fish
-
-# Aliases
-# AWS CLI (Load in credentials from Pass)
-function load_aws_cli
-  set -Ux AWS_ACCESS_KEY_ID (pass show Development/AWS/Personal | sed -n 's/access-key-id: //p')
-  set -Ux AWS_SECRET_ACCESS_KEY (pass show Development/AWS/Personal | sed -n 's/secret-access-key: //p')
-
-  command aws $argv
-end
-
-# AWS CDK (Load in credentials from Pass)
-function load_aws_cdk
-  set -x AWS_ACCESS_KEY_ID (pass show Development/AWS/Personal | sed -n 's/access-key-id: //p')
-  set -x AWS_SECRET_ACCESS_KEY (pass show Development/AWS/Personal | sed -n 's/secret-access-key: //p')
-
-  command cdk $argv
-end
-
-alias aws="load_aws_cli"
-alias cdk="load_aws_cdk"
+# 1Password SSH Agent
+set -x SSH_AUTH_SOCK $HOME/.1password/agent.sock
 
 # Go
 set -x GOPATH $HOME/go
 set -x PATH $PATH /usr/local/go/bin $GOPATH/bin
-
-# NPM and Yarn
-set PATH $HOME/.npm-global/bin $PATH
-set PATH (yarn global bin) $PATH
 
 # Random scripts/binaries
 set PATH $HOME/bin $PATH
